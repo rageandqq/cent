55906a62ca4d36de2972748f13077cefa249a793
rageandqq <chitley.sameer@gmail.com>
2014-02-22 16:25:18 -0500

    Created EditActivity and XML.

---
+++ src/com/rageandqq/Cent/EditActivity.java
@@ -0,0 +1,14 @@
+package com.rageandqq.Cent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sameer on 2/22/2014.
+ */
+public class EditActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit);
+    }
+}
